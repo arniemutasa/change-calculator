37a9fc21cd4b4908a793de95723185c25dc32ab0
Arnie Mutasa <bantuhacker@gmail.com>
2024-10-16 14:49:18 +0200

    Added helmet package for security headers and rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { ChangeModule } from './change/change.module';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), HttpModule, ChangeModule ],
+  }), 
+
+  ThrottlerModule.forRoot([{
+    ttl: 60000,
+    limit: 5,
+  }]),
+  
+  
+  HttpModule, ChangeModule ],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class AppModule {}
