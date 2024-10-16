97d5c9cd6a12fdd34596bc28d4c7eb04f82632dd
Arnie Mutasa <bantuhacker@gmail.com>
2024-10-16 14:00:36 +0200

    App module cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChangeModule } from './change/change.module';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
@@ -9,7 +7,7 @@ import { HttpModule } from '@nestjs/axios';
   imports: [ConfigModule.forRoot({
     isGlobal: true
   }), HttpModule, ChangeModule ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
